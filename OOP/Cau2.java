5b3ed5b04146a86facb0860503bfc685ac0a0ec8
HoangLong30112 <longcoinbase@gmail.com>
2025-07-29 22:27:54 +0700

    Test tiep ở sach 4 Cau2.java

--- OOP/Cau2.java
+++ OOP/Cau2.java
@@ -8,7 +8,7 @@ class Cau2{
         Sach sach1 = new Sach("B001", "Lập trình Java", 100.0, "Nguyễn Văn A");
         Sach sach2 = new Sach("B002", "Cấu trúc dữ liệu", 120.0, "Trần Thị B");
         Sach sach3 = new Sach("B003", "Hệ điều hành", 150.0, "Lê Văn C");
-        Sach sach4 = new Sach("B004", "Test git", 150.0, "Lê Long");
+        Sach sach4 = new Sach("B004", "Test git", 150.0, "Lê Long test test tiếp");
 
         // Tạo đơn hàng
         DonHang donhang = new DonHang("DH001", new Date());
