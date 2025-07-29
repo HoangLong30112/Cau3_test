97958c46afb07ecd64d2d344befd675f27359932
HoangLong30112 <longcoinbase@gmail.com>
2025-07-29 22:21:39 +0700

    Them sach 3 Cau2.java

--- OOP/Cau2.java
+++ OOP/Cau2.java
@@ -8,12 +8,14 @@ class Cau2{
         Sach sach1 = new Sach("B001", "Lập trình Java", 100.0, "Nguyễn Văn A");
         Sach sach2 = new Sach("B002", "Cấu trúc dữ liệu", 120.0, "Trần Thị B");
         Sach sach3 = new Sach("B003", "Hệ điều hành", 150.0, "Lê Văn C");
+        Sach sach4 = new Sach("B004", "Test git", 150.0, "Lê Long");
 
         // Tạo đơn hàng
         DonHang donhang = new DonHang("DH001", new Date());
         donhang.them_Sach(sach1);
         donhang.them_Sach(sach2);
         donhang.them_Sach(sach3);
+        donhang.them_Sach(sach4);
 
         // In thông tin đơn hàng
         donhang.printOrderInfo();
